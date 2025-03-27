f8bb805cf8eb84d81177ca8bfb26139dc3b2e1d5
Sathish Kumar S <146180365+S-ATHISH14@users.noreply.github.com>
2025-03-27 12:30:08 +0530

    Delete hotel reservation directory

--- hotel reservation/src/model/Tester.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Tester {
-    public static void main(String[] args){
-        Customer customer=new Customer("first","second","j@domain.com");
-        System.out.println(customer);
-    }
-}
